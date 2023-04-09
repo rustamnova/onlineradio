e84488979496db21a53339e20b090f0e23a5fdce
rustamnova <novamedia777@gmail.com>
2023-04-09 20:57:26 +0300

    add pydub lib

--- main.py
+++ main.py
@@ -1,41 +1,24 @@
-import os
+import requests
 import pygame
-import subprocess
-import time
+import os
+from pydub import AudioSegment
 
-def play_radio(url):
-    # удаляем временные файлы, если они есть
-    if os.path.isfile('temp.mp3'):
-        os.remove('temp.mp3')
-    if os.path.isfile('temp.wav'):
-        os.remove('temp.wav')
+def play_radio(radio_url):
+    r = requests.get(radio_url, stream=True)
+    with open('temp.mp3', 'wb') as f:
+        for block in r.iter_content(1024):
+            f.write(block)
 
-    # загружаем поток и сохраняем его в mp3 файл
-    subprocess.call(['ffmpeg', '-i', url, '-f', 'mp3', '-ab', '128k', '-vn', '-y', 'temp.mp3'])
+    audio = AudioSegment.from_mp3('temp.mp3')
+    audio.export('temp.wav', format='wav')
 
-    # конвертируем mp3 файл в wav формат
-    subprocess.call(['ffmpeg', '-i', os.path.abspath('temp.mp3'), '-ar', '44100', '-ac', '2', '-f', 'wav', '-y', 'temp.wav'])
-
-    # инициализируем Pygame
-    pygame.init()
-
-    # загружаем и проигрываем файл в wav формате
-    try:
-        pygame.mixer.music.load(os.path.abspath('temp.wav'))
+    pygame.mixer.init()
+    temp_wav_path = os.path.abspath('temp.wav')
+    if os.path.exists(temp_wav_path):
+        pygame.mixer.music.load(temp_wav_path)
         pygame.mixer.music.play()
-        # ждем, пока поток не закончится
-        while pygame.mixer.music.get_busy():
-            time.sleep(1)
-    except pygame.error:
-        print('Ошибка проигрывания музыки')
+    else:
+        print("File not found: ", temp_wav_path)
 
-    # очищаем Pygame и удаляем временные файлы
-    pygame.mixer.quit()
-    pygame.quit()
-    if os.path.isfile('temp.mp3'):
-        os.remove('temp.mp3')
-    if os.path.isfile('temp.wav'):
-        os.remove('temp.wav')
-
-# тестируем функцию на радио-потоке
 play_radio("http://stream.radioreklama.bg:80/radio1rock128.mp3")
+
